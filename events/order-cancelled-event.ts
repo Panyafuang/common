d892cb757c54ff6c81420ee60a52cacac1cd9df9
Thanadilok Panyafuang <thanadilok.x@gmail.com>
2025-08-04 22:14:03 +0700

    Updates

--- events/order-cancelled-event.ts
+++ events/order-cancelled-event.ts
@@ -1,13 +1,17 @@
 import { Subjects } from "./subjects"
 
+// Interface สำหรับข้อมูลตั๋วแต่ละใบใน Event
+interface TicketInfo {
+    scheduleId: string;
+    seatNumber: string;
+    price: number;
+}
 
 export interface IOrderCancelledEvent {
     subject: Subjects.OrderCancelled,
     data: {
         id: string;
         version: number; // the version number of the order itselt
-        ticket: {
-            id: string;
-        }
+        tickets: TicketInfo[];
     }
 }
